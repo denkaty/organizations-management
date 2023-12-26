1fc2cd5bb605f84db7e87a80ed15f846be64671a
denkaty <denisselim2003@gmail.com>
2023-12-26 18:03:48 +0200

    Register new services

--- src/OrganizationsAPI/Organizations.Presentation.API/Program.cs
+++ src/OrganizationsAPI/Organizations.Presentation.API/Program.cs
@@ -1,3 +1,8 @@
+using Organizations.Business.Abstraction.Factories;
+using Organizations.Business.Abstraction.Services;
+using Organizations.Business.AutoMapper;
+using Organizations.Business.Factories;
+using Organizations.Business.Services;
 using Organizations.Data.Abstraction.DatabaseContexts;
 using Organizations.Data.Abstraction.OrganizationsDatabase.Configuraters;
 using Organizations.Data.Abstraction.OrganizationsDatabase.Repositories;
@@ -13,6 +18,7 @@ var organizationsDatabaseOptions = builder.Configuration.GetSection(nameof(Organ
 
 builder.Services.Configure<OrganizationsDatabaseOptions>(organizationsDatabaseOptions);
 
+builder.Services.AddAutoMapper(typeof(OrganizationsProfile));
 builder.Services.AddTransient<IOrganizationsDatabaseConnectionValidator, OrganizationsDatabaseConnectionValidator>();
 builder.Services.AddTransient<IOrganizationsDatabaseTableExistenceChecker, OrganizationsDatabaseTableExistenceChecker>();
 builder.Services.AddTransient<IOrganizationsDatabaseTableInitializer, OrganizationsDatabaseTableInitializer>();
@@ -22,7 +28,10 @@ builder.Services.AddScoped<IOrganizationsDatabaseCountryRepository, Organization
 builder.Services.AddScoped<IOrganizationsDatabaseIndustryRepository, OrganizationsDatabaseIndustryRepository>();
 builder.Services.AddScoped<IOrganizationsDatabaseOrganizationRepository, OrganizationsDatabaseOrganizationRepository>();
 builder.Services.AddScoped<IOrganizationsDatabaseOrganizationIndustryRepository, OrganizationsDatabaseOrganizationIndustryRepository>();
+builder.Services.AddScoped<IAPIResultFactory, APIResultFactory>();
 builder.Services.AddScoped<IOrganizationsContext, OrganizationsContext>();
+builder.Services.AddScoped<ICountryService, CountryService>();
+builder.Services.AddScoped<IOrganizationService, OrganizationService>();
 
 builder.Services.AddControllers();
 
