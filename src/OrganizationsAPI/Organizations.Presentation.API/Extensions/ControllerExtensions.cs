4831c4111413b6c04b3aee5e7d550839f1ccf2f6
denkaty <denisselim2003@gmail.com>
2023-12-26 18:03:33 +0200

    Add 'ControllerExtensions' class

---
+++ src/OrganizationsAPI/Organizations.Presentation.API/Extensions/ControllerExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Organizations.Business.Models;
+using Organizations.Business.Models.Enums;
+using Organizations.Business.Models.Results.Base;
+using System.Net;
+
+namespace Organizations.Presentation.API.Extensions
+{
+	public static class ControllerExtensions
+	{
+		public static IActionResult HandleResponse<T>(this ControllerBase controller, IAPIResult<T> apiResult)
+		{
+			switch (apiResult.StatusCode)
+			{
+				case OrganizationsAPIStatusCode.NotFound:
+					return controller.NotFound();
+				case OrganizationsAPIStatusCode.OK:
+					return controller.Ok(apiResult.Data);
+				case OrganizationsAPIStatusCode.NoContent:
+					return controller.NoContent();
+				default:
+					return controller.BadRequest();
+			}
+		}
+
+	}
+}
