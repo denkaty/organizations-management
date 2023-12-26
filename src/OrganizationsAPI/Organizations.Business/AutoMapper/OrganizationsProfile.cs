5d45d17d193c94d17b65057937f5ab64e952907c
denkaty <denisselim2003@gmail.com>
2023-12-26 17:56:39 +0200

    Add profiles

---
+++ src/OrganizationsAPI/Organizations.Business/AutoMapper/OrganizationsProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Organizations.Business.Models.DTOs.Country;
+using Organizations.Business.Models.DTOs.Organization;
+using Organizations.Data.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organizations.Business.AutoMapper
+{
+    public class OrganizationsProfile : Profile
+	{
+        public OrganizationsProfile()
+        {
+			CreateMap<CreateCountryDTO, Country>().ReverseMap();
+            CreateMap<Country, ResultCountryDTO>().ReverseMap();
+            CreateMap<UpdateCountryDTO, Country>().ReverseMap();
+
+			CreateMap<CreateOrganizationDTO, Organization>().ReverseMap();
+        }
+    }
+}
