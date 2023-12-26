001f759b6716a3e4fc1e96a396fdb3679a075102
denkaty <denisselim2003@gmail.com>
2023-12-26 17:59:05 +0200

    Create new Status Code enum

---
+++ src/OrganizationsAPI/Organizations.Business.Models/Enums/OrganizationsAPIStatusCode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organizations.Business.Models.Enums
+{
+	public enum OrganizationsAPIStatusCode
+	{
+		OK,
+		Created,
+		NoContent,
+		NotFound,
+		BadRequest,
+		Unauthorized,
+		Forbidden
+	}
+}
