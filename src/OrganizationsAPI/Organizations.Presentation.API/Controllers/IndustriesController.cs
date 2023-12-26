f9cc7fd49a21eb37966ac72e3f38a8f2a3ff1ef3
denkaty <denisselim2003@gmail.com>
2023-12-26 18:08:26 +0200

    Add 'IndustriesController' implementation

---
+++ src/OrganizationsAPI/Organizations.Presentation.API/Controllers/IndustriesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Organizations.Business.Abstraction.Services;
+using Organizations.Business.Models.DTOs.Country;
+using Organizations.Business.Models.DTOs.Industry;
+using Organizations.Presentation.API.Extensions;
+
+namespace Organizations.Presentation.API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class IndustriesController : ControllerBase
+	{
+		private readonly IIndustryService _industryService;
+		public IndustriesController(IIndustryService industryService)
+		{
+			_industryService = industryService;
+		}
+
+		[HttpPost]
+		public IActionResult Create([FromBody] CreateIndustryDTO createIndustryDTO)
+		{
+			var apiResult = _industryService.Create(createIndustryDTO);
+
+			return this.HandleResponse(apiResult);
+		}
+
+		[HttpGet("id/{id}")]
+		public IActionResult GetById([FromRoute] string id)
+		{
+			var apiResult = _industryService.GetById(id);
+
+			return this.HandleResponse(apiResult);
+		}
+
+		[HttpGet("name/{name}")]
+		public IActionResult GetByName([FromRoute] string name)
+		{
+			var apiResult = _industryService.GetByName(name);
+
+			return this.HandleResponse(apiResult);
+		}
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			var apiResult = _industryService.GetAll();
+
+			return this.HandleResponse(apiResult);
+
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult UpdateById([FromRoute] string id, [FromBody] UpdateIndustryDTO updateIndustryDTO)
+		{
+			var apiResult = _industryService.UpdateById(id, updateIndustryDTO);
+
+			return this.HandleResponse(apiResult);
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult DeleteById([FromRoute] string id)
+		{
+			var apiResult = _industryService.DeleteById(id);
+
+			return this.HandleResponse(apiResult);
+		}
+	}
+}
