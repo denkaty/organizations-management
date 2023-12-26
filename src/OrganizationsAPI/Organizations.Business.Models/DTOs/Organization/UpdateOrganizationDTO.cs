55efefb671906ca26ab13fdffccd6d22dc547f30
denkaty <denisselim2003@gmail.com>
2023-12-26 17:58:47 +0200

    Create new DTOs

---
+++ src/OrganizationsAPI/Organizations.Business.Models/DTOs/Organization/UpdateOrganizationDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organizations.Business.Models.DTOs.Organization
+{
+    public class UpdateOrganizationDTO
+    {
+        public string Name { get; set; }
+        public string Website { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public int Founded { get; set; }
+        public ICollection<string> Industries { get; set; }
+        public int Employees { get; set; }
+    }
+}
