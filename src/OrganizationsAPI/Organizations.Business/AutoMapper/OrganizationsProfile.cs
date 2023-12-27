f145587259e27444097c61c59714cab5a9d9218b
denkaty <denisselim2003@gmail.com>
2023-12-28 00:30:22 +0200

    Add mapping configurations

--- src/OrganizationsAPI/Organizations.Business/AutoMapper/OrganizationsProfile.cs
+++ src/OrganizationsAPI/Organizations.Business/AutoMapper/OrganizationsProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Organizations.Business.Models.DTOs.Country;
+using Organizations.Business.Models.DTOs.Industry;
 using Organizations.Business.Models.DTOs.Organization;
 using Organizations.Data.Models.Entities;
 using System;
@@ -18,7 +19,21 @@ namespace Organizations.Business.AutoMapper
             CreateMap<Country, ResultCountryDTO>().ReverseMap();
             CreateMap<UpdateCountryDTO, Country>().ReverseMap();
 
-			CreateMap<CreateOrganizationDTO, Organization>().ReverseMap();
-        }
-    }
+            CreateMap<CreateIndustryDTO, Industry>().ReverseMap();
+            CreateMap<Industry, ResultIndustryDTO>().ReverseMap();
+            CreateMap<UpdateIndustryDTO, Industry>().ReverseMap();
+
+			CreateMap<CreateOrganizationDTO, Organization>()
+			   .ForMember(dest => dest.Id, opt => opt.Ignore())
+			   .ForMember(dest => dest.OrganizationId, opt => opt.MapFrom(src => src.OrganizationId))
+			   .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+			   .ForMember(dest => dest.Website, opt => opt.MapFrom(src => src.Website))
+			   .ForMember(dest => dest.CountryId, opt => opt.Ignore())
+			   .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+			   .ForMember(dest => dest.Founded, opt => opt.MapFrom(src => src.Founded))
+			   .ForMember(dest => dest.Employees, opt => opt.MapFrom(src => src.Employees));
+			CreateMap<Organization, ResultOrganizationDTO>().ReverseMap();
+			CreateMap<UpdateOrganizationDTO, Organization>().ReverseMap();
+		}
+	}
 }
