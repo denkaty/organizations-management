ff0edc7a34b62d419c76d18713b0ff9f0281bf62
denkaty <denisselim2003@gmail.com>
2023-12-28 00:31:34 +0200

    Refactoring

--- src/OrganizationsAPI/Organizations.Business/Services/IndustryService.cs
+++ src/OrganizationsAPI/Organizations.Business/Services/IndustryService.cs
@@ -1,9 +1,12 @@
 ﻿using AutoMapper;
 using Organizations.Business.Abstraction.Factories;
 using Organizations.Business.Abstraction.Services;
+using Organizations.Business.Models.DTOs.Country;
+using Organizations.Business.Models.DTOs.Industry;
 using Organizations.Business.Models.DTOs.Industry;
 using Organizations.Business.Models.Results.Base;
 using Organizations.Data.Abstraction.OrganizationsDatabase.Repositories;
+using Organizations.Data.Models.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,32 +31,83 @@ namespace Organizations.Business.Services
 		}
 		public IAPIResult<ResultIndustryDTO> Create(CreateIndustryDTO createIndustryDTO)
 		{
-			throw new NotImplementedException();
-		}
+			Industry? existingIndustry = _industryRepository.GetByName(createIndustryDTO.Name);
+			ResultIndustryDTO resultIndustryDTO;
+			if (existingIndustry == null)
+			{
+				Industry createdIndustry = _mapper.Map<Industry>(createIndustryDTO);
+				_industryRepository.Create(createdIndustry);
 
-		public IAPIResult<ResultIndustryDTO> DeleteById(string id)
-		{
-			throw new NotImplementedException();
-		}
+				resultIndustryDTO = _mapper.Map<ResultIndustryDTO>(createdIndustry);
+				return _apiResultFactory.GetOKResult(resultIndustryDTO);
+			}
 
-		public IAPIResult<ICollection<ResultIndustryDTO>> GetAll()
-		{
-			throw new NotImplementedException();
+			resultIndustryDTO = _mapper.Map<ResultIndustryDTO>(existingIndustry);
+			return _apiResultFactory.GetOKResult(resultIndustryDTO);
 		}
 
 		public IAPIResult<ResultIndustryDTO> GetById(string id)
 		{
-			throw new NotImplementedException();
+			Industry? existingIndustry = _industryRepository.GetById(id);
+			if (existingIndustry is null)
+			{
+				return _apiResultFactory.GetNotFoundResult<ResultIndustryDTO>();
+			}
+
+			ResultIndustryDTO resultIndustryDTO = _mapper.Map<ResultIndustryDTO>(existingIndustry);
+			return _apiResultFactory.GetOKResult(resultIndustryDTO);
 		}
 
 		public IAPIResult<ResultIndustryDTO> GetByName(string name)
 		{
-			throw new NotImplementedException();
+			Industry? existingIndustry = _industryRepository.GetByName(name);
+			if (existingIndustry is null)
+			{
+				return _apiResultFactory.GetNotFoundResult<ResultIndustryDTO>();
+			}
+
+			ResultIndustryDTO resultIndustryDTO = _mapper.Map<ResultIndustryDTO>(existingIndustry);
+			return _apiResultFactory.GetOKResult(resultIndustryDTO);
 		}
 
+		public IAPIResult<ICollection<ResultIndustryDTO>> GetAll()
+		{
+			var existingIndustries = _industryRepository.GetAll();
+
+			var resultIndustryDTOs = _mapper.Map<ICollection<ResultIndustryDTO>>(existingIndustries);
+
+			return _apiResultFactory.GetOKResult(resultIndustryDTOs);
+		}
 		public IAPIResult<ResultIndustryDTO> UpdateById(string id, UpdateIndustryDTO updateIndustryDTO)
 		{
-			throw new NotImplementedException();
+			Industry? existingIndustry = _industryRepository.GetById(id);
+
+			if (existingIndustry is null)
+			{
+				return _apiResultFactory.GetNotFoundResult<ResultIndustryDTO>();
+			}
+
+			_mapper.Map(updateIndustryDTO, existingIndustry);
+
+			_industryRepository.UpdateById(id, existingIndustry);
+
+			var resultIndustryDTO = _mapper.Map<ResultIndustryDTO>(existingIndustry);
+
+			return _apiResultFactory.GetOKResult(resultIndustryDTO);
 		}
+
+		public IAPIResult<ResultIndustryDTO> DeleteById(string id)
+		{
+			Industry? existingIndustry = _industryRepository.GetById(id);
+
+			if (existingIndustry is null)
+			{
+				return _apiResultFactory.GetNotFoundResult<ResultIndustryDTO>();
+			}
+
+			_industryRepository.DeleteById(id);
+			return _apiResultFactory.GetNoContentResult<ResultIndustryDTO>();
+		}
+		
 	}
 }
