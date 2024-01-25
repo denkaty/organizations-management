895dda40d4838b20ad2f4dc87ba024a10c494e20
denkaty <denisselim2003@gmail.com>
2024-01-26 01:08:42 +0200

    Add order attribute

---
+++ src/OrganizationsAPI/Organizations.Data.Models/CustomAttributes/OrderAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organizations.Data.Models.CustomAttributes
+{
+	public class OrderAttribute : Attribute
+	{
+		public int Order { get; }
+
+		public OrderAttribute(int order)
+		{
+			Order = order;
+		}
+	}
+}
