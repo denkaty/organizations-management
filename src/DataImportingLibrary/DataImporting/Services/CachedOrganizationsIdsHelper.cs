d61cf72c632c47d84e8494e7aa923cd63395ed55
denkaty <denisselim2003@gmail.com>
2024-01-26 01:00:08 +0200

    Add caching for organizations ids

---
+++ src/DataImportingLibrary/DataImporting/Services/CachedOrganizationsIdsHelper.cs
@@ -0,0 +1,35 @@
+﻿using DataImporting.Abstraction.Services;
+using Organizations.Data.Abstraction.DatabaseContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImporting.Services
+{
+	public class CachedOrganizationsIdsHelper : ICachedOrganizationsIdsHelper
+	{
+		private HashSet<string> _organizationsIdsSet;
+
+		public void AddId(string id)
+		{
+			_organizationsIdsSet.Add(id);
+		}
+
+		public bool ContainsId(string id)
+		{
+			return _organizationsIdsSet.Contains(id);
+		}
+
+		public void LoadData(IOrganizationsContext organizationsContext)
+		{
+			_organizationsIdsSet = organizationsContext.Organizations.GetAllIds().ToHashSet();
+		}
+
+		public void RemoveId(string id)
+		{
+			_organizationsIdsSet.Remove(id);
+		}
+	}
+}
