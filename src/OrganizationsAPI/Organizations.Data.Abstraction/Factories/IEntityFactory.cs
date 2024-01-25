17f4ee27dbfa4b233014fcbd4654fd483693dca3
denkaty <denisselim2003@gmail.com>
2024-01-26 01:18:25 +0200

    Add EntityFactory

---
+++ src/OrganizationsAPI/Organizations.Data.Abstraction/Factories/IEntityFactory.cs
@@ -0,0 +1,16 @@
+﻿using Organizations.Data.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organizations.Data.Abstraction.Factories
+{
+	public interface IEntityFactory
+	{
+		public Country CreateCountry(string name);
+		public Industry CreateIndustry(string name);
+		public OrganizationIndustry CreateOrganizationIndustry(string organizationId, string industryId);
+	}
+}
