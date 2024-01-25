ef98c52298f9b98c7d164b136848c427ca8874b6
denkaty <denisselim2003@gmail.com>
2024-01-26 01:05:20 +0200

    Add data loading background service

---
+++ src/OrganizationsAPI/Organizations.Presentation.API/BackgroundServices/DataLoadBackgroundService.cs
@@ -0,0 +1,29 @@
+﻿
+using DataImporting.Abstraction.Services;
+using Organizations.Data.Abstraction.DatabaseContexts;
+
+namespace Organizations.Presentation.API.BackgroundServices
+{
+	public class DataLoadBackgroundService : BackgroundService
+	{
+		private readonly IServiceProvider _serviceProvider;
+		private readonly ICachedOrganizationsIdsHelper _cachedOrganizationsIdsHelper;
+        public DataLoadBackgroundService(IServiceProvider serviceProvider, 
+									     ICachedOrganizationsIdsHelper organizationsIdsHelper)
+        {
+			_serviceProvider = serviceProvider;
+			_cachedOrganizationsIdsHelper = organizationsIdsHelper;
+		}
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			using (var scope = _serviceProvider.CreateScope())
+			{
+				var organizationsContext = scope.ServiceProvider.GetRequiredService<IOrganizationsContext>();
+
+				_cachedOrganizationsIdsHelper.LoadData(organizationsContext);
+			}
+
+			return Task.CompletedTask;
+		}
+	}
+}
