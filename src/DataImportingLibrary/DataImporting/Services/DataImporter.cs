984d219b094d1a004cdd0e806f03671043daace1
denkaty <denisselim2003@gmail.com>
2024-01-26 01:01:57 +0200

    Add bulk insert

--- src/DataImportingLibrary/DataImporting/Services/DataImporter.cs
+++ src/DataImportingLibrary/DataImporting/Services/DataImporter.cs
@@ -1,13 +1,18 @@
 ﻿using AutoMapper;
 using DataImporting.Abstraction.Services;
 using DataImporting.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
 using Organizations.Business.Abstraction.Services;
 using Organizations.Business.Models.DTOs.Country;
 using Organizations.Business.Models.Results.Base;
 using Organizations.Data.Abstraction.DatabaseContexts;
+using Organizations.Data.Models.CustomAttributes;
 using Organizations.Data.Models.Entities;
+using Organizations.Data.Models.Options;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,68 +21,76 @@ namespace DataImporting.Services
 {
 	public class DataImporter : IDataImporter
 	{
-		private readonly IOrganizationsContext _organizationsContext;
-		private readonly IMapper _mapper;
-
-		public DataImporter(IOrganizationsContext organizationsContext, 
-							IMapper mapper)
+		private readonly string _connectionString;
+		public DataImporter(IOptions<OrganizationsDatabaseOptions> options)
 		{
-			_organizationsContext = organizationsContext;
-			_mapper = mapper;
+			_connectionString = options.Value.ConnectionString;
 		}
 
-		public void ImportData(ICollection<NormalizedOrganization> organizations)
+		public void ImportData(BulkedDataWrapper bulkedDataWrapper)
 		{
-			foreach (var currentOrganization in organizations)
+			ProcessBulkInsert(bulkedDataWrapper.BulkedCountries);
+			ProcessBulkInsert(bulkedDataWrapper.BulkedIndustries);
+			ProcessBulkInsert(bulkedDataWrapper.BulkedOrganizations);
+			ProcessBulkInsert(bulkedDataWrapper.BulkedOrganizationsIndustries);
+		}
+		private void ProcessBulkInsert<T>(ICollection<T> data) where T : class
+		{
+			BulkInsert<T>(ConvertToDataTable(data));
+		}
+		private DataTable ConvertToDataTable<T>(ICollection<T> data) where T : class
+		{
+			DataTable dataTable = new();
+
+			if (data == null || data.Count == 0)
 			{
-				Organization? existingOrganization = _organizationsContext.Organizations.GetByName(currentOrganization.Name);
-				if (existingOrganization != null)
+				return dataTable;
+			}
+
+			var properties = typeof(T)
+				.GetProperties()
+				.OrderBy(p => ((OrderAttribute)p.GetCustomAttributes(typeof(OrderAttribute), false).FirstOrDefault())?.Order ?? int.MaxValue)
+				.ToArray();
+
+			foreach (var property in properties)
+			{
+				dataTable.Columns.Add(property.Name);
+			}
+
+			foreach (var item in data)
+			{
+				DataRow row = dataTable.NewRow();
+
+				foreach (var property in properties)
 				{
-					continue;
+					row[property.Name] = property.GetValue(item) ?? DBNull.Value;
 				}
 
-				Organization newOrganization = _mapper.Map<Organization>(currentOrganization);
+				dataTable.Rows.Add(row);
+			}
 
+			return dataTable;
+		}
+		private void BulkInsert<T>(DataTable dataTable) where T : class
+		{
+			using (var connection = new SqlConnection(_connectionString))
+			{
+				connection.Open();
+				SqlTransaction sqlTransaction = connection.BeginTransaction();
 
-				Country? existingCountry = _organizationsContext.Countries.GetByName(currentOrganization.Country);
-				if (existingCountry == null)
+				using (SqlBulkCopy bulkCopy = new SqlBulkCopy(connection, SqlBulkCopyOptions.TableLock, sqlTransaction))
 				{
-					Country newCountry = _mapper.Map<Country>(currentOrganization); //does it use the Country - : id
-					_organizationsContext.Countries.Create(newCountry);
-					newOrganization.CountryId = newCountry.Id;
+					bulkCopy.DestinationTableName = typeof(T).Name;
+					bulkCopy.BulkCopyTimeout = 0;
+
+					bulkCopy.WriteToServer(dataTable);
 				}
-				else
-				{
-					newOrganization.CountryId = existingCountry.Id;
-				}
-				_organizationsContext.Organizations.Create(newOrganization);
 
-
-				foreach (var industryName in currentOrganization.Industries)
-				{
-					Industry? existingIndustry = _organizationsContext.Industries.GetByName(industryName);
-					if(existingIndustry == null)
-					{
-						Industry newIndustry = new Industry();
-						newIndustry.Name = industryName;
-						_organizationsContext.Industries.Create(newIndustry);
-
-						OrganizationIndustry organizationIndustry = new OrganizationIndustry();
-						organizationIndustry.Organization_Id = newOrganization.Id;
-						organizationIndustry.Industry_Id = newIndustry.Id;
-						_organizationsContext.OrganizationsIndustries.Create(organizationIndustry);
-					}
-					else
-					{
-						OrganizationIndustry organizationIndustry = new OrganizationIndustry();
-						organizationIndustry.Organization_Id = newOrganization.Id;
-						organizationIndustry.Industry_Id = existingIndustry.Id;
-						_organizationsContext.OrganizationsIndustries.Create(organizationIndustry);
-					}
-				}
-				
-
+				sqlTransaction.Commit();
 			}
 		}
+
+		
 	}
+
 }
